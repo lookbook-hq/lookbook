8adae6605e7f342ca980d348f4c6ffad66c2d695
Mark Perkins <mark@allmarkedup.com>
2022-03-18 13:41:14 +0000

    Markdown renderer improvements

--- lib/lookbook/markdown.rb
+++ lib/lookbook/markdown.rb
@@ -4,12 +4,16 @@ module Lookbook
       markdown = Redcarpet::Markdown.new(Renderer, {
         tables: true,
         fenced_code_blocks: true,
-        disable_indented_code_blocks: true
+        disable_indented_code_blocks: true,
+        strikethrough: true,
+        highlight: true,
+        with_toc_data: true
       })
       markdown.render(text).html_safe
     end
 
     def self.highlight(source, language = nil, opts = {})
+      source&.gsub!("&gt;", "<")&.gsub!("&lt;", ">")
       language ||= "ruby"
       formatter = Lookbook::CodeFormatter.new(opts)
       lexer = Rouge::Lexer.find(language)
