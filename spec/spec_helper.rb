9b11b7945beb8cd7723d64e8753ac8d5b586330b
Mark Perkins <mark@allmarkedup.com>
2022-05-29 21:45:12 +0100

    Set up factorybot

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,15 +7,19 @@ Combustion.initialize! :action_controller, :action_view do
   config.view_component.view_component_path = "../app/components"
 end
 
+require "factory_bot"
 require "view_component/test_helpers"
 require "rspec/rails"
 require "capybara/rspec"
 require_relative "support/component_spec_helper"
 require_relative "support/request_spec_helper"
+require_relative "support/factories"
 
 RSpec.configure do |config|
   config.include ViewComponent::TestHelpers, type: :component
   config.include Capybara::RSpecMatchers, type: :component
+  config.include FactoryBot::Syntax::Methods
+
   config.include ComponentSpecHelper, type: :component
   config.include RequestSpecHelper, type: :request
 
---
+++ spec/support/factories.rb
@@ -0,0 +1,48 @@
+FactoryBot.define do
+  factory :class_code_object, class: "YARD::CodeObjects::ClassObject" do
+    transient do
+      file { Rails.root.join("example_class.rb") }
+    end
+
+    namespace { :root }
+    name { :ExampleClass }
+
+    after(:build) do |code_object, evaluator|
+      if evaluator.file.present?
+        code_object.add_file(evaluator.file)
+      end
+    end
+
+    initialize_with { new(namespace, name) }
+
+    factory :preview_code_object do
+      transient do
+        file { Rails.root.join("example_component_preview.rb") }
+      end
+      namespace { :root }
+      name { :ExampleComponentPreview }
+    end
+  end
+
+  factory :preview_tag, class: "YARD::Tags::Tag" do
+    transient do
+      file { nil }
+    end
+
+    tag_name { :example }
+    name { "example" }
+    types { [] }
+    text { nil }
+
+    after(:build) do |tag, evaluator|
+      tag.object = build(:preview_code_object, file: evaluator.file)
+    end
+
+    initialize_with { new(tag_name, text, types, name) }
+
+    factory :param_tag do
+      tag_name { :param }
+      name { "param_name" }
+    end
+  end
+end
