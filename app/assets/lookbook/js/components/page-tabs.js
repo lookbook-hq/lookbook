36a17ec35e9c97fc63a77cb5b8be4491893d565a
Leigh Halliday <leighhalliday@gmail.com>
2022-06-08 09:57:03 -0400

    Removing unused JS

--- app/assets/lookbook/js/components/page-tabs.js
+++ app/assets/lookbook/js/components/page-tabs.js
@@ -4,12 +4,6 @@ export default function pageTabs() {
 
     changeTab(tabName) {
       this.active = tabName;
-      document
-        .querySelector(`#tab-${tabName}`)
-        .querySelectorAll("iframe")
-        .forEach((iframe) => {
-          // iframe.click();
-        });
     },
   };
 }
