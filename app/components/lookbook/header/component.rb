740d7edabd7f893aa3081319cb734e19054eca2c
Mark Perkins <mark@allmarkedup.com>
2022-06-17 00:19:59 +0100

    Add debug menu visibility setting

--- app/components/lookbook/header/component.rb
+++ app/components/lookbook/header/component.rb
@@ -2,6 +2,11 @@ module Lookbook
   class Header::Component < Lookbook::BaseComponent
     renders_one :branding
 
+    def initialize(debug_menu: false, **html_attrs)
+      @debug_menu = debug_menu
+      super(**html_attrs)
+    end
+
     def landing_path
       helpers.landing_path if defined? helpers.landing_path
     end
