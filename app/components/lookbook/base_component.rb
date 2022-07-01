b310ebc1dfbc61114d5845e76556dddcc718e993
Mark Perkins <mark@allmarkedup.com>
2022-07-01 23:05:26 +0100

    Use strings for Alpine attribute names

--- app/components/lookbook/base_component.rb
+++ app/components/lookbook/base_component.rb
@@ -10,11 +10,13 @@ module Lookbook
 
     def render_component_tag(tag = :div, **attrs, &block)
       merged_classes = class_names(attrs[:class], @html_attrs[:class])
-      merge_attrs = @html_attrs.except(:class).deep_merge(attrs.except(:class))
+      merged_attrs = @html_attrs.except(:class).deep_merge(attrs.except(:class))
+
+
       render Lookbook::TagComponent.new(tag: tag,
         name: component_name,
-        **merge_attrs,
-        x_data: prepare_alpine_data(attrs[:x_data]),
+        **merged_attrs,
+        "x-data": prepare_alpine_data(merged_attrs[:"x-data"]),
         class: merged_classes), &block
     end
 
@@ -31,7 +33,7 @@ module Lookbook
     end
 
     def prepare_alpine_data(x_data = nil)
-      alpine_component_name = x_data || @html_attrs&.dig(:x_data) || alpine_component
+      alpine_component_name = x_data || @html_attrs&.dig(:"x-data") || alpine_component
       if alpine_component_name.present?
         args = Array.wrap(alpine_data)
         args.any? ? "#{alpine_component_name}(#{safe_join(args).gsub("&quot;", "'").tr("\"", "'")})" : alpine_component_name
--- app/components/lookbook/tag_component.rb
+++ app/components/lookbook/tag_component.rb
@@ -6,22 +6,14 @@ module Lookbook
       @tag = tag
       html_attrs[:data] ||= {}
       html_attrs[:data][:component] = name if name.present?
-      html_attrs[:x_cloak] = true if cloak == true
-      @html_attrs = convert_attrs(html_attrs)
+      html_attrs["x-cloak"] = true if cloak == true
+      @html_attrs = html_attrs
     end
 
     def call
       tag.public_send(@tag, **@html_attrs) do
         content
       end
-    end
-
-    protected
-
-    def convert_attrs(html_attrs)
-      html_attrs.map do |name, value|
-        [name.to_s.tr("_", "-"), value]
-      end.to_h
     end
   end
 end
