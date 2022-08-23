df089c2edf3cb4c928283044cb9bd530c28ee850
Mark Perkins <mark@allmarkedup.com>
2022-08-23 21:06:10 +0100

    Manually Include page helpers in PagesController

--- app/controllers/lookbook/pages_controller.rb
+++ app/controllers/lookbook/pages_controller.rb
@@ -1,6 +1,7 @@
 module Lookbook
   class PagesController < ApplicationController
     helper_method :page_controller
+    helper Lookbook::PageHelper
 
     def self.controller_path
       "lookbook/pages"
