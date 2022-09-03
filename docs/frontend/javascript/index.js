111a1b3727cc6f558e3789fe5bfde6fde6ced6c1
Mark Perkins <mark@allmarkedup.com>
2022-09-03 17:03:22 +0100

    Fix sidebar scroll position in docs

--- docs/frontend/javascript/index.js
+++ docs/frontend/javascript/index.js
@@ -1,9 +1,27 @@
 import "index.css";
 import Alpine from "alpinejs";
+import * as Turbo from "@hotwired/turbo";
 
 // Import all JavaScript & CSS files from src/_components
 import "bridgetownComponents/**/*.{js,jsx,js.rb,css}";
 
-window.Alpine = Alpine;
+const scrollPositions = {};
 
+document.addEventListener("turbo:before-render", function () {
+  document
+    .querySelectorAll("[data-turbo-maintain-scroll]")
+    .forEach(function (element) {
+      scrollPositions[element.id] = element.scrollTop;
+    });
+});
+
+document.addEventListener("turbo:render", function () {
+  document
+    .querySelectorAll("[data-turbo-maintain-scroll]")
+    .forEach(function (element) {
+      element.scrollTop = scrollPositions[element.id];
+    });
+});
+
+window.Alpine = Alpine;
 Alpine.start();
