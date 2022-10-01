4361340a57a02810e2095b2840475d062f962740
Mark Perkins <mark@allmarkedup.com>
2022-10-01 01:08:07 +0100

    feat(panels): support providing panel partial path as second arg when defining custom panels

--- lib/lookbook/panels.rb
+++ lib/lookbook/panels.rb
@@ -1,15 +1,25 @@
 module Lookbook
   module Panels
-    def define_panel(name, opts = {})
-      Lookbook.config.define_inspector_panel(name, opts)
+    def define_panel(name, *args)
+      Lookbook.config.define_inspector_panel(name, extract_opts(args))
     end
 
-    def amend_panel(name, opts = {})
-      Lookbook.amend_inspector_panel(name, opts)
+    def amend_panel(name, *args)
+      Lookbook.amend_inspector_panel(name, extract_opts(args))
     end
 
     def remove_panel(name)
       Lookbook.remove_inspector_panel(name)
+    end
+
+    def extract_opts(args)
+      if args.many?
+        opts = args[1]
+        opts[:partial] = args[0]
+        opts
+      elsif args.any?
+        args[0].is_a?(String) ? {partial: args[0]} : args[0]
+      end
     end
   end
 end
