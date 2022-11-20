59d6dcd9546274fdcaa2066678148b06debe31ce
Mark Perkins <mark@allmarkedup.com>
2022-11-20 22:39:37 +0000

    refactor(lang): load language definitions from config file

--- lib/lookbook/lang.rb
+++ lib/lookbook/lang.rb
@@ -1,65 +1,23 @@
 module Lookbook
   module Lang
     class << self
-      LANGUAGES = [
-        {
-          name: "ruby",
-          ext: ".rb",
-          label: "Ruby",
-          comment: "# %s"
-        },
-        {
-          name: "html",
-          ext: ".html",
-          label: "HTML",
-          comment: "<!-- %s -->"
-        },
-        {
-          name: "erb",
-          ext: ".erb",
-          label: "ERB",
-          comment: "<%%# %s %%>"
-        },
-        {
-          name: "haml",
-          ext: ".haml",
-          label: "Haml",
-          comment: "<!-- %s -->"
-        },
-        {
-          name: "slim",
-          ext: ".slim",
-          label: "Slim",
-          comment: "<!-- %s -->"
-        },
-        {
-          name: "tsx",
-          ext: ".tsx",
-          label: "TypeScript",
-          comment: "// %s"
-        },
-        {
-          name: "js",
-          ext: ".js",
-          label: "JavasScript",
-          comment: "// %s"
-        },
-        {
-          name: "css",
-          ext: ".css",
-          label: "CSS",
-          comment: "/* %s */"
-        }
-      ]
+      CONFIG_FILE = "config/languages.yml"
+      LANGUAGES = [] # retained for backwards compatability
 
       def find(name)
-        LANGUAGES.find { |l| l[:name] == name.to_s }
+        languages.find { |l| l[:name] == name.to_s }
       end
 
       def guess(path, fallback_name = nil)
         ext = File.extname(path)
-        lang = LANGUAGES.find { |l| l[:ext] == ext }
+        lang = languages.find { |l| l[:ext] == ext }
         lang || (find(fallback_name) if fallback_name)
+      end
+
+      protected
+
+      def languages
+        @_languages ||= [*ConfigLoader.call(CONFIG_FILE).definitions, *LANGUAGES]
       end
     end
   end
