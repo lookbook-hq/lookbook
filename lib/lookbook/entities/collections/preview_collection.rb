895cb29cf29a2a9b31410dcc1e6c3b42a5170fde
Mark Perkins <mark@allmarkedup.com>
2022-12-05 17:38:16 +0000

    refactor(previews): instatiate correct preview class

--- lib/lookbook/entities/collections/preview_collection.rb
+++ lib/lookbook/entities/collections/preview_collection.rb
@@ -43,13 +43,13 @@ module Lookbook
 
     def self.preview_from_code_object(code_object)
       klass = code_object.path.constantize
-      Preview.new(code_object) if preview_class?(klass)
+      PreviewEntity.new(code_object) if preview_class?(klass)
     rescue => exception
       Lookbook.logger.error exception.to_s
       nil
     end
 
-    def self.preview_class?(klass) 
+    def self.preview_class?(klass)
       if klass.ancestors.include?(ViewComponent::Preview) || klass.ancestors.include?(Lookbook::Preview)
         !klass.respond_to?(:abstract_class) || klass.abstract_class != true
       end
