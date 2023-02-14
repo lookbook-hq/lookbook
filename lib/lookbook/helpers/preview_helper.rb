cb9dba24ba9cb29f9a348e8ef3193455fe12cd02
Mark Perkins <mark@allmarkedup.com>
2023-02-14 18:45:54 +0000

    fix(previews): fix url_for route resolving in previews
    re #328

--- lib/lookbook/helpers/preview_helper.rb
+++ lib/lookbook/helpers/preview_helper.rb
@@ -10,5 +10,9 @@ module Lookbook
     def lookbook_data(key, fallback = nil)
       Lookbook.data.fetch(key.to_sym, fallback)
     end
+
+    def url_for(*args)
+      main_app.url_for(*args)
+    end
   end
 end
