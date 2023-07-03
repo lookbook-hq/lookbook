ce863fbf40d906d38a764bee22e4ec5bdca41200
Stephen Ierodiaconou <stevegeek@gmail.com>
2023-07-03 23:08:10 +0200

    Remove the TOC only renderer

--- lib/lookbook/services/markdown/markdown_with_toc_renderer.rb
+++ lib/lookbook/services/markdown/markdown_with_toc_renderer.rb
@@ -3,9 +3,23 @@ require "redcarpet"
 module Lookbook
   class MarkdownWithTocRenderer < MarkdownRenderer
     def call
-      html = super
-      toc_html = MarkdownTocRenderer.call(text)
-      toc_html + "<hr>".html_safe + html
+      render_toc + "<hr>".html_safe + render_content
+    end
+
+    private
+
+    def render_content
+      md_renderer = Redcarpet::Markdown.new(LookbookMarkdownRenderer.new(with_toc_data: true), opts)
+      md_renderer.render(clean_text).html_safe
+    end
+
+    def render_toc
+      toc_renderer = Redcarpet::Markdown.new(Redcarpet::Render::HTML_TOC.new)
+      toc_renderer.render(clean_text).html_safe
+    end
+
+    def clean_text
+      @_clean_text ||= ActionViewAnnotationsStripper.call(text)
     end
   end
 end
