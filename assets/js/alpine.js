e24b87867a994f6598505f37f8b8e01e3700484a
Mark Perkins <mark@allmarkedup.com>
2024-02-12 19:41:51 +0000

    Start on app layout build

--- assets/js/alpine.js
+++ assets/js/alpine.js
@@ -1,5 +1,10 @@
 import Alpine from "alpinejs";
+import morph from "@alpinejs/morph";
+import persist from "@alpinejs/persist";
 
 window.Alpine = Alpine;
+
+Alpine.plugin(morph);
+Alpine.plugin(persist);
 
 Alpine.start();
--- assets/js/shoelace.js
+++ assets/js/shoelace.js
@@ -1,6 +1,7 @@
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
 
-import "@shoelace-style/shoelace/dist/components/button/button.js";
-import "@shoelace-style/shoelace/dist/components/icon/icon.js";
+import "@shoelace-style/shoelace/dist/components/split-panel/split-panel.js";
+import "@shoelace-style/shoelace/dist/components/tree/tree.js";
+import "@shoelace-style/shoelace/dist/components/tree-item/tree-item.js";
 
 setBasePath("/lookbook-dev/shoelace");
