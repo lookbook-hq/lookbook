1007dd7f401d9eee77d6807b664aaec2477a4190
Mark Perkins <mark@allmarkedup.com>
2024-02-17 23:45:37 +0000

    Tab group tweaks

--- assets/js/shoelace/index.js
+++ assets/js/shoelace/index.js
@@ -1,5 +1,12 @@
 import { setBasePath } from "@shoelace-style/shoelace/dist/utilities/base-path.js";
 
+import "@shoelace-style/shoelace/dist/components/icon/icon.js";
+
+import "@shoelace-style/shoelace/dist/components/button/button.js";
+import "@shoelace-style/shoelace/dist/components/icon-button/icon-button.js";
+import "@shoelace-style/shoelace/dist/components/copy-button/copy-button.js";
+import "@shoelace-style/shoelace/dist/components/button-group/button-group.js";
+
 import "@shoelace-style/shoelace/dist/components/split-panel/split-panel.js";
 
 import "@shoelace-style/shoelace/dist/components/tree/tree.js";
@@ -8,5 +15,8 @@ import "@shoelace-style/shoelace/dist/components/tree-item/tree-item.js";
 import "@shoelace-style/shoelace/dist/components/tab-group/tab-group.js";
 import "@shoelace-style/shoelace/dist/components/tab/tab.js";
 import "@shoelace-style/shoelace/dist/components/tab-panel/tab-panel.js";
+
+import "@shoelace-style/shoelace/dist/components/tooltip/tooltip.js";
+import "@shoelace-style/shoelace/dist/components/resize-observer/resize-observer.js";
 
 setBasePath("/lookbook-dev/shoelace");
