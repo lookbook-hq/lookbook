fcbffdda9548f8cfc8bb228c013025d8747f7fbc
Mark Perkins <mark@allmarkedup.com>
2024-03-05 17:18:24 +0000

    Remove codejar package

--- app/components/lookbook/ui/code/code.js
+++ app/components/lookbook/ui/code/code.js
@@ -1,24 +1,10 @@
 import AlpineComponent from "@js/alpine/component";
-import { CodeJar } from "codejar";
-import Prism from "prismjs";
-import "prismjs/components/prism-json";
-import "prismjs/components/prism-haml";
-import "prismjs/components/prism-liquid";
-import "prismjs/components/prism-ruby";
-import "prismjs/components/prism-css";
-import "prismjs/components/prism-css-extras";
-import "prismjs/components/prism-markup";
-import "prismjs/components/prism-markup-templating";
-import "prismjs/components/prism-erb";
+import Prism from "@js/prism";
 
 export default AlpineComponent("code", () => {
   return {
     init() {
-      const editor = CodeJar(this.$refs.editor, (el) =>
-        Prism.highlightElement(el)
-      );
-      this.$refs.editor.setAttribute("contenteditable", "false");
-      editor.updateCode(this.$refs.source.innerText);
+      Prism.highlightElement(this.$refs.code);
     },
   };
 });
---
+++ assets/js/prism.js
@@ -0,0 +1,14 @@
+import Prism from "prismjs";
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-haml";
+import "prismjs/components/prism-liquid";
+import "prismjs/components/prism-ruby";
+import "prismjs/components/prism-css";
+import "prismjs/components/prism-css-extras";
+import "prismjs/components/prism-markup";
+import "prismjs/components/prism-markup-templating";
+import "prismjs/components/prism-erb";
+
+Prism.manual = true;
+
+export default Prism;
