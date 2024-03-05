acbfe6549b3c2c5ab1559a85314a3020e73b5558
Mark Perkins <mark@allmarkedup.com>
2024-03-05 21:19:07 +0000

    Add basic debug controller

---
+++ app/controllers/lookbook/debug_controller.rb
@@ -0,0 +1,8 @@
+module Lookbook
+  class DebugController < ApplicationController
+    layout "lookbook/debug"
+
+    def show
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Lookbook::Engine.routes.draw do
   get "/previews/:preview/:target", to: "previews#inspect", as: :inspect_target
   get "/previews/:preview/:target/preview", to: "previews#preview", as: :preview_target
 
+  get "/debug", to: "debug#show", as: :debug
+
   resources :events, only: [:index]
 end
 
