758e5a8fe08845ea9bc6d5dd1badcd1237145247
Mark Perkins <mark@allmarkedup.com>
2024-03-30 15:40:25 +0000

    Fix icon button target

--- app/components/lookbook/ui/app/icon_button/icon_button.rb
+++ app/components/lookbook/ui/app/icon_button/icon_button.rb
@@ -1,12 +1,13 @@
 module Lookbook
   module UI
     class IconButton < BaseComponent
-      attr_reader :icon_name, :tooltip, :click, :show, :href, :label
+      attr_reader :icon_name, :tooltip, :click, :show, :href, :label, :target
 
-      def initialize(icon:, tooltip: nil, href: nil, click: nil, show: nil, label: nil, **kwargs)
+      def initialize(icon:, tooltip: nil, href: nil, click: nil, show: nil, label: nil, target: nil, **kwargs)
         @icon_name = icon.to_s.tr("_", "-")
         @tooltip = tooltip
         @href = href
+        @target = target
         @click = click
         @show = show
         @label = label || @tooltip
