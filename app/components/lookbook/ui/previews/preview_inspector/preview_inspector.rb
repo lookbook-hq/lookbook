8dbc2efd06351b88443a83a37d31cac1c35a3a81
Mark Perkins <mark@allmarkedup.com>
2024-04-25 00:11:34 +0100

    Fix embed updating on param change

--- app/components/lookbook/ui/previews/preview_inspector/preview_inspector.rb
+++ app/components/lookbook/ui/previews/preview_inspector/preview_inspector.rb
@@ -1,11 +1,12 @@
 module Lookbook
   module UI
     class PreviewInspector < BaseComponent
-      attr_reader :preview, :target, :preview_panels, :drawer_panels, :display_options, :display_options_values
+      attr_reader :preview, :target, :preview_url, :preview_panels, :drawer_panels, :display_options, :display_options_values
 
-      def initialize(preview:, target:, preview_panels: [], drawer_panels: [], display_options: {}, display_options_values: {}, **kwargs)
+      def initialize(preview:, target:, preview_url:, preview_panels: [], drawer_panels: [], display_options: {}, display_options_values: {}, **kwargs)
         @preview = preview
         @target = target
+        @preview_url = preview_url
         @preview_panels = preview_panels
         @drawer_panels = drawer_panels
         @display_options = display_options
