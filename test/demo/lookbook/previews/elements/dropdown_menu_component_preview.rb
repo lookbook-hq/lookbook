bd33f6345ad445f50d49e8571a5705b7fbe97dc5
Mark Perkins <mark@allmarkedup.com>
2024-05-09 18:25:52 +0100

    Fix description

--- test/demo/lookbook/previews/elements/dropdown_menu_component_preview.rb
+++ test/demo/lookbook/previews/elements/dropdown_menu_component_preview.rb
@@ -1,7 +1,7 @@
 module Elements
   # A dropdown menu component built with `Phlex`.
   #
-  # The default scenario uses a `@display` tag to customise the color of the preview background.
+  # The default scenario uses a `@display` tag to customise the max width of the layout.
   class DropdownMenuComponentPreview < Lookbook::Preview
     # @display max_width "600px"
     def default
