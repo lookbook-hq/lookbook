ed0530495a5e7d27fb223908d04f255e931a53c5
Mark Perkins <mark@allmarkedup.com>
2024-05-18 22:34:38 +0100

    Handle invalid permalink UUIDs

--- app/controllers/lookbook/application_controller.rb
+++ app/controllers/lookbook/application_controller.rb
@@ -7,27 +7,9 @@ module Lookbook
 
     rescue_from ActionController::RoutingError, with: :not_found
 
-    def permalink
-      type = params[:uuid].split("_")&.first
-      if type.present?
-        redirect_url = case type.to_sym
-        when :page
-          page = Pages.find { _1.uuid == params[:uuid] }
-          page_path(page, request.query_parameters)
-        when :preview
-          preview = Previews.find { _1.uuid == params[:uuid] }
-          preview_page_path(preview, request.query_parameters)
-        when :target
-          target = Previews.inspector_targets.find { _1.uuid == params[:uuid] }
-          inspect_target_path(target.preview, target, request.query_parameters)
-        else
-          raise ActionController::RoutingError, "Could not resolve permalink '#{params[:uuid]}'"
-        end
-
-        redirect_to redirect_url
-      else
-        raise ActionController::RoutingError, "Could not resolve permalink '#{params[:uuid]}'"
-      end
+    def not_found(error = nil)
+      @error = error
+      render :not_found, status: :not_found
     end
 
     protected
@@ -40,11 +22,6 @@ module Lookbook
       @previews = Previews
       @notifications = Engine.notifications
       @events_endpoint = events_path if Engine.watch_files?
-    end
-
-    def not_found(error)
-      @error = error
-      render :not_found, status: :not_found
     end
   end
 end
--- app/controllers/lookbook/permalink_controller.rb
+++ app/controllers/lookbook/permalink_controller.rb
@@ -6,21 +6,19 @@ module Lookbook
         redirect_url = case type.to_sym
         when :page
           page = Pages.find { _1.uuid == params[:uuid] }
-          page_path(page, request.query_parameters)
+          page_path(page, request.query_parameters) if page
         when :preview
           preview = Previews.find { _1.uuid == params[:uuid] }
-          preview_page_path(preview, request.query_parameters)
+          preview_page_path(preview, request.query_parameters) if preview
         when :target
           target = Previews.inspector_targets.find { _1.uuid == params[:uuid] }
-          inspect_target_path(target.preview, target, request.query_parameters)
-        else
-          raise ActionController::RoutingError, "Could not resolve permalink '#{params[:uuid]}'"
+          inspect_target_path(target.preview, target, request.query_parameters) if target
         end
 
-        redirect_to redirect_url
-      else
-        raise ActionController::RoutingError, "Could not resolve permalink '#{params[:uuid]}'"
+        redirect_to redirect_url and return if redirect_url
       end
+
+      raise ActionController::RoutingError, "Could not resolve permalink '#{params[:uuid]}'"
     end
   end
 end
