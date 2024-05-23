6d6445d09f56437bd9d03a4b6c465b639a9c9073
Mark Perkins <mark@allmarkedup.com>
2024-05-23 22:12:00 +0100

    Improve user mailer preview example

--- test/demo/app/mailers/user_mailer.rb
+++ test/demo/app/mailers/user_mailer.rb
@@ -1,6 +1,8 @@
 class UserMailer < ApplicationMailer
   def welcome
     @user_name = params[:user_name]
+    @body_text = params[:body_text]
+    @button_label = params[:button_label]
     mail(to: "user@example.com", subject: "Welcome")
   end
 end
--- test/demo/lookbook/previews/emails/user_mailer_preview.rb
+++ test/demo/lookbook/previews/emails/user_mailer_preview.rb
@@ -3,8 +3,23 @@ module Emails
   class UserMailerPreview < ActionMailer::Preview
     # @label Welcome email
     # @param user_name
-    def welcome(user_name: "Alice")
-      UserMailer.with(user_name: user_name).welcome
+    # @param body_text textarea
+    # @param button_label
+    def welcome(user_name: "Alice", body_text: default_body_text, button_label: "Click me")
+      UserMailer.with(user_name: user_name, body_text: body_text, button_label: button_label).welcome
+    end
+
+    private
+
+    def default_body_text
+      <<-TEXT.strip
+        Donec ut est finibus, sodales dui et, pulvinar ipsum. Vivamus euismod mi et egestas sollicitudin.
+
+        Quisque dictum ornare augue, id interdum libero ullamcorper sed. Suspendisse molestie pulvinar interdum. Donec nec finibus dolor.
+
+        Mauris nec nisl at tellus eleifend faucibus. In hendrerit non sapien eget lacinia.
+
+      TEXT
     end
   end
 end
