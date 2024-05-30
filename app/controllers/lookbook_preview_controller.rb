730b2f6a88bdf2725094d1153119b9d4191d7e54
Mark Perkins <mark@allmarkedup.com>
2024-05-30 11:00:10 +0100

    ficup controller

--- app/controllers/lookbook_preview_controller.rb
+++ app/controllers/lookbook_preview_controller.rb
@@ -1,6 +1,4 @@
 require "rails/application_controller"
 class LookbookPreviewController < Rails::ApplicationController
   include Lookbook::PreviewControllerActions
-  helper Lookbook::AssetHelper
-  helper Lookbook::PreviewHelper
 end
