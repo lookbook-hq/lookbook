62a1e83a3dc5b75055c69bdb56a19b7fe172710a
Mark Perkins <mark@allmarkedup.com>
2024-10-23 21:45:59 +0100

    Fix broken placeholder image URL

--- spec/dummy/test/components/previews/multiline_tag_component_preview.rb
+++ spec/dummy/test/components/previews/multiline_tag_component_preview.rb
@@ -1,5 +1,5 @@
 class MultilineTagComponentPreview < ViewComponent::Preview
   def default
-    render MultilineTagComponent.new src: "https://placekitten.com/200/200"
+    render MultilineTagComponent.new src: "https://loremflickr.com/200/200"
   end
 end
