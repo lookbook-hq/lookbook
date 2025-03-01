6035d73877adc7df6470ff036346280c138650bf
Mark Perkins <mark@allmarkedup.com>
2025-03-01 19:42:46 +0000

    SSE tweaks

--- app/controllers/lookbook/events_controller.rb
+++ app/controllers/lookbook/events_controller.rb
@@ -9,22 +9,20 @@ module Lookbook
       response.headers["Last-Modified"] = Time.now.httpdate
 
       response.headers["rack.hijack"] = proc do |stream|
+        queue = Thread::Queue.new
+        trigger_background_loop(queue)
+
         Thread.new do
           sse = SSE.new(stream, retry: 300, event: "open")
-          last_update_event_sent = Engine.updated_at
-
           loop do
-            if last_update_event_sent.before?(Engine.updated_at)
-              debug("events: sending update event to clients")
-              sse.write({type: "update", updated_at: Engine.updated_at}, event: "event")
-              last_update_event_sent = Engine.updated_at
-            end
-            sleep 0.5
+            data = queue.pop
+            sse.write(data, event: "event")
           end
-        rescue ClientDisconnected, Errno::EPIPE, Errno::ECONNRESET, IOError
-          sse.close
+        rescue ClientDisconnected, Errno::EPIPE, Errno::ECONNRESET, IOError => err
+          warn("events: #{err}")
         ensure
           sse.close
+          stream.close
         end
       end
 
@@ -33,6 +31,22 @@ module Lookbook
 
     def ping
       render html: Engine.updated_at
+    end
+
+    private
+
+    def trigger_background_loop(queue)
+      Thread.new do
+        last_update_event_sent = Engine.updated_at
+        loop do
+          if last_update_event_sent.before?(Engine.updated_at)
+            debug("events: sending update event to clients")
+            queue.push({type: "update", updated_at: Engine.updated_at})
+            last_update_event_sent = Engine.updated_at
+          end
+          sleep 0.5
+        end
+      end
     end
   end
 end
