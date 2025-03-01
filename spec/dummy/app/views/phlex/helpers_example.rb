3cc145e1646cb581eb60124048b6760f655c911c
Mark Perkins <mark@allmarkedup.com>
2025-03-01 11:28:32 +0000

    Replace deprecated `helpers` call in Phlex test component

--- spec/dummy/app/views/phlex/helpers_example.rb
+++ spec/dummy/app/views/phlex/helpers_example.rb
@@ -1,7 +1,7 @@
 module Views::Phlex
   class HelpersExample < Phlex::HTML
     def view_template
-      a(href: helpers.url_for(:root)) { "click here" }
+      a(href: view_context.url_for(:root)) { "click here" }
     end
   end
 end
