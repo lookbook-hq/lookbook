2756aa39d232f484f4c8473ead780e53ff5d23dd
Mark Perkins <mark@allmarkedup.com>
2025-03-05 17:35:02 +0000

    Allow dashes in .local domain names for demo app

--- test/demo/config/environments/development.rb
+++ test/demo/config/environments/development.rb
@@ -51,6 +51,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << /[a-z0-9]+\.local/
+  config.hosts << /[a-z0-9\-]+\.local/
   config.hosts << /[a-z0-9\-]+\.localcan\.dev/
 end
