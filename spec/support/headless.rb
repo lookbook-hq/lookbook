f6c9701192fd880a33aad4f2aaba64a5475b3960
Mark Perkins <mark@allmarkedup.com>
2025-07-23 21:09:32 +0100

    Explicitly set window size in  Capyara chrome driver

--- spec/support/headless.rb
+++ spec/support/headless.rb
@@ -5,6 +5,7 @@ Capybara.register_driver :selenium_chrome_headless do |app|
   options_key = Capybara::Selenium::Driver::CAPS_VERSION.satisfied_by?(version) ? :capabilities : :options
   browser_options = ::Selenium::WebDriver::Chrome::Options.new.tap do |opts|
     opts.add_argument("--headless")
+    opts.add_argument("--window-size=1400,1000")
     opts.add_argument("--disable-gpu") if Gem.win_platform?
     # Workaround https://bugs.chromium.org/p/chromedriver/issues/detail?id=2650&q=load&sort=-id&colspec=ID%20Status%20Pri%20Owner%20Summary
     opts.add_argument("--disable-site-isolation-trials")
