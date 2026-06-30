38eedbd5836ad41a318c50aed4ff4a6f5c6c9994
LiuLiu <liuliu-dev@github.com>
2026-06-30 21:21:41 +0000

    Fix XSS in display options field alpine data interpolation

--- app/components/lookbook/display_options/field/component.rb
+++ app/components/lookbook/display_options/field/component.rb
@@ -17,7 +17,9 @@ module Lookbook
       protected
 
       def alpine_data
-        "{name: '#{name}', value: '#{value}'}"
+        escaped_name = html_escape(json_escape(name.to_s))
+        escaped_value = html_escape(json_escape(value.to_s).gsub("\n", '\n'))
+        "{name: \"#{escaped_name}\", value: \"#{escaped_value}\"}"
       end
 
       def alpine_component
